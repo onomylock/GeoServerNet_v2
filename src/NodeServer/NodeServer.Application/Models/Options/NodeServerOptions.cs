0e11bea298438fa5e6cf36e7e9c3cc73bdbda9c1
onomylock <onomylock@outlook.com>
2025-02-09 11:36:23 +0300

    add jwt

--- src/NodeServer/NodeServer.Application/Models/Options/NodeServerOptions.cs
+++ src/NodeServer/NodeServer.Application/Models/Options/NodeServerOptions.cs
@@ -2,9 +2,9 @@ using System.ComponentModel.DataAnnotations;
 using Shared.Common.Models;
 using Shared.Common.Models.Options;
 
-namespace MasterServer.Application.Models.Options;
+namespace NodeServer.Application.Models.Options;
 
-public class MasterServerOptions : CommonServiceOptions
+public class NodeServerOptions : CommonServiceOptions
 {
     /// <summary>
     ///     HTTP Secure Cookies
@@ -17,4 +17,7 @@ public class MasterServerOptions : CommonServiceOptions
     ///     CORS allowed origins
     /// </summary>
     public string[] CorsAllowedOrigins { get; set; }
+    
+    [Required] public UriData MasterServerJsonWebTokenExpiredChannel { get; set; }
+    [Required] public UriData MasterServerJsonWebTokenAuthenticationChannel { get; set; }
 }
